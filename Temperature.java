568a55c570d9c205af28e94ca34ce97efbd8b369
TheMax370 <69474247+TheMax370@users.noreply.github.com>
2021-11-26 19:04:44 -0400

    Add files via upload
    Agregado clases TemperatureScale y temperature

--- Temperature.java
+++ Temperature.java
@@ -1,5 +1,26 @@
 public class Temperature {
     public static void main(String[] args) {
-      System.out.println("Hello World");
+      System.out.println(temperature(5.8f));
+    }
+    
+    public static string TemperatureScale(String Scale){
+       
+      if(Scale == "Celsius") {
+        return "Celsius";
+      }
+       
+      else if (Scale == "Fahrenheit"){
+        return "Fahrenheit";
+      }
+
+      else if (Scale == "Kelvin"){
+         return "Kelvin";
+      }
+    }
+
+    public static float temperature(float temp){
+         
+      return 5.7f;
+
     }
   }
